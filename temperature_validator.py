28f699f1413322befaea40f4a5fe1c6e58166d78
Mansour Boroumand <peyman.borumand@gmail.com>
2026-08-31 13:10:32 +0200

    Add temperature validation

---
+++ temperature_validator.py
@@ -0,0 +1,8 @@
+measured_temperature = 24.5
+min_temperature = 20.0
+max_temperature = 30.0
+
+if min_temperature <= measured_temperature <= max_temperature:
+    print("PASS")
+else:
+    print("FAIL")
