0f312d4c892b812b74b09bbdb2ddd56da0f87cc6
Mansour Boroumand <peyman.borumand@gmail.com>
2026-09-07 16:14:38 +0200

    Remove obsolete temperature validator

--- temperature_validator.py
+++
@@ -1,8 +0,0 @@
-measured_temperature = 24.5
-min_temperature = 20.0
-max_temperature = 30.0
-
-if min_temperature <= measured_temperature <= max_temperature:
-    print("PASS")
-else:
-    print("FAIL")
